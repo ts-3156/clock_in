e2147f1bef8d6faea6580d2a840242868c8d621d
ts-3156 <ts_3156@yahoo.co.jp>
2018-12-14 23:08:10 +0900

    Post in thread

--- slack.py
+++ slack.py
@@ -1,14 +1,20 @@
 import os
+import threading
 import requests
+
 
 URL = 'https://slack.com/api/chat.postMessage'
 TOKEN = os.getenv("SLACK_TOKEN", "Not set")
 CHANNEL = os.getenv("SLACK_CHANNEL", "Not set")
 
 
-def post(text):
+def _post(text):
     res = requests.post(URL, data={'token': TOKEN, 'channel': CHANNEL, 'text': text})
     print(res.text)
+
+
+def post(text):
+    threading.Thread(target=_post, args=(text,))
 
 
 if __name__ == '__main__':
